e4b0dc092d3ee747a067155c2e10e4f1e4b81c99
root <root@DESKTOP-8EBFE1N.localdomain>
2026-07-07 01:30:29 -0300

    Feature: adicionado suporte a envio de mensagens de audio

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,21 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.get('/', (req, res) => res.send('Backend Ativo!'));
 
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+
+const io = new Server(server, { 
+    path: "/socket.io/",
+    maxHttpBufferSize: 1e7, // Aumenta o limite para ~10MB para aguentar os áudios
+    cors: { 
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true
+    } 
+});
 
 io.on('connection', (socket) => {
     socket.on('envia_mensagem', (dados) => {
